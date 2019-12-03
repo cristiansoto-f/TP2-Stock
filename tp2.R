25ddb609d35fc8e3b244af5475d4caab3ba1aa83
cristiansoto-f <56857490+cristiansoto-f@users.noreply.github.com>
2019-12-02 22:13:09 -0300

    Update tp2.R

--- tp2.R
+++ tp2.R
@@ -1,87 +1,161 @@
 #install.packages("quantmod")
 #install.packages("tidyquant")
+#install.packages("tidyverse")
 library(tidyquant)
 library(tidyverse)
 library(quantmod)
 library(dplyr)
 library(ggplot2)
 
-#Obtención de Datos
-#Bolsas
+fecha.comienzo = "2014-01-01"
+fecha.fin = "2019-12-01"
+
+#####FUNCIONES####
+
+delete_na_values = function(c_exchglist)
+{
+  if(is_tibble(index) && is_tibble(c_exchglist))
+  {
+  
+  c_exchglist <- c_exchglist %>%
+    mutate(adjusted = replace(adjusted,
+                              is.na(adjusted),
+                              0))
+  }
+  else return("Parámetros mal ingresados")
+}
+usd_ars2 = delete_na_values(Merval, usd_ars)
+
+actualizar_precios = function(index, c_exchglist)
+{
+  
+  for (i in 1:nrow(index))
+  {
+    date = index[i, "date"]
+    exchange_rate = c_exchglist %>%
+      select(adjusted) %>%
+      filter(c_exchglist$date == date)
+    if(as.numeric(exchange_rate) == 0)
+    {
+      index[i, "adjusted"] = NA
+    }
+    else
+      index[i, "adjusted"] = index[i, "adjusted"]/exchange_rate
+  }
+  print(index)
+}
+
+graficar_precios = function(index, title, y, x)
+{
+  #Procurar utilizar datos actualizados
+  index %>%
+    ggplot(aes(x = date, y = adjusted)) +
+    geom_line() +
+    labs(title = title, y = y, x = x) + 
+    theme_tq()
+  
+}
+
+graph_index_returns_monthly <- function(index, title, y, x)
+{
+  index_returns_monthly <- index %>%
+  tq_transmute(select     = adjusted, 
+               mutate_fun = periodReturn, 
+               period     = "monthly", 
+               col_rename = "returns")
+  
+  index_returns_monthly %>%
+    ggplot(aes(x = date, y = index_returns_monthly$returns)) +
+    geom_line() +
+    labs(title = title, y = y, x = x) + 
+    theme_tq()
+  
+}
+####FIN FUNCIONES####
+####Recopilación de Datos####
+Merval = actualizar_precios(Merval, usd_ars2)
+usd_ars <- usd_ars %>%
+  mutate(adjusted = replace(adjusted,
+                            is.na(adjusted),
+                            0))
+#####.Bolsas#####
 Merval <- c("^MERV") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-01",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 #EEUU
 SP500 <-  c("^GSPC") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #JapÃ³n
 Nikkei225 <- c("^N225") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #UK
 FTSE100 <- c("^FTSE") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #Alemania
 DAX <- c("^GDAXI") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #España
 IBEX35 <- c("^IBEX") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #China
 SHANGAI <- c("000001.SS") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #Mexico
 IPCMEX <- c("^MXX") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #India
 SENSEX <- c("^BSESN") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
 #Brasil
 IBOVESPA <- c("^BVSP") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
-#Acciones
+#####.Acciones#####
 stockMerval = c("ALUA.BA", "BMA.BA", "BBAR.BA", "BYMA.BA", "CVH.BA", "CEPU.BA",
                 "CRES.BA", "EDN.BA", "GGAL.BA", "VALO.BA", "SUPV.BA", "HARG.BA",
                 "PAMP.BA", "COME.BA", "TECO2.BA", "TXAR.BA", "TRAN.BA", "TGNO4.BA",
                 "TGSU2.BA", "YPFD.BA") %>%
-  tq_get(get = "stock.prices", complete_cases = T)
+  tq_get(get = "stock.prices",
+         from = fecha.comienzo,
+         to = fecha.fin,
+         complete_cases = T)
 
 #write.csv(stockMerval, "stockMerval")
 
@@ -90,53 +164,14 @@ stockMerval = c("ALUA.BA", "BMA.BA", "BBAR.BA", "BYMA.BA", "CVH.BA", "CEPU.BA",
 #plot(Merval, na.rm = T)
 #write.csv(Merval, "")
 
-
-
-#Obtener exchange_rates
+#####.Exchange rates#####
 usd_ars = c("USDARS=X") %>%
   tq_get(get = "stock.prices",
-         from = "2014-01-01",
-         to = "2019-12-02",
+         from = fecha.comienzo,
+         to = fecha.fin,
          complete_cases = T)
 
-
-#Sustitución de valores nulos para el loop
-usd_ars <- usd_ars %>%
-  mutate(adjusted = replace(adjusted,
-                            is.na(adjusted),
-                            0))
-
-for (i in 1:nrow(Merval))
-{
-  date = Merval[i, "date"]
-  exchange_rate = usd_ars %>%
-    select(adjusted) %>%
-    filter(usd_ars$date == date)
-  if(as.numeric(exchange_rate) == 0)
-  {
-    Merval[i, "adjusted"] = NA
-  }
-  else
-    Merval[i, "adjusted"] = Merval[i, "adjusted"]/exchange_rate
-}
-
+#####Graficos####
 #Grafico del merval en dolares
-Merval %>%
-  ggplot(aes(x = date, y = adjusted)) +
-  geom_line() +
-  labs(title = "Merval en USD", y = "Puntos", x = "") + 
-  theme_tq()
-
-merval_returns_monthly <- Merval %>%
-  tq_transmute(select     = adjusted, 
-               mutate_fun = periodReturn, 
-               period     = "monthly", 
-               col_rename = "Returns")
-merval_returns_monthly
-
-merval_returns_monthly %>%
-  ggplot(aes(x = date, y = merval_returns_monthly$Returns)) +
-  geom_line() +
-  labs(title = "Merval", y = "Retornos en U$D", x = "") + 
-  theme_tq()
-
+graficar_precios(Merval, "Merval en Dólares", "Puntos", "")
+graph_index_returns_monthly(Merval, "Merval", "Retornos", "")
