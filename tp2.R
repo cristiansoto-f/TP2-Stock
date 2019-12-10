d349645836919e87017491ac8c7f96a95c59c158
cristiansoto-f <56857490+cristiansoto-f@users.noreply.github.com>
2019-12-10 13:58:44 -0300

    Update tp2.R

--- tp2.R
+++ tp2.R
@@ -11,6 +11,12 @@ library(plotly)
 library(chron)
 library(caret)
 library(e1071)
+library(quantmod)
+library(PerformanceAnalytics)
+library(PortfolioAnalytics)
+library(ROI)
+require(ROI.plugin.glpk)
+require(ROI.plugin.quadprog)
 
 fecha.comienzo = "2009-12-01"
 fecha.fin = "2019-12-01"
@@ -251,3 +257,50 @@ summary(modelo)
 d_merval_test$pred<-predict(modelo, d_merval_test, type= "response")
 plot(d_merval_test$merval, col="blue")
 points(d_merval_test$pred,col = "red")
+
+
+
+#Optimizacion de portafolio
+stockMerval = c("BMA.BA", "BBAR.BA", "CVH.BA",
+                "CRES.BA", "EDN.BA", "GGAL.BA", "VALO.BA",
+                "PAMP.BA",  "TECO2.BA", "TRAN.BA",
+                "TGSU2.BA", "YPFD.BA") %>%
+  tq_get(get = "stock.prices",
+         from = fecha.comienzo,
+         to = fecha.fin,
+         complete_cases = T)
+
+portfolioReturns <- na.omit(ROC(stockMerval))
+
+
+tickers <- c("BMA.BA", "CRES.BA", "EDN.BA", "GGAL.BA", "VALO.BA",
+             "PAMP.BA",  "TECO2.BA", "TRAN.BA", "TGSU2.BA", "YPFD.BA")
+
+portfolioPrices <- NULL
+for(ticker in tickers) {
+  portfolioPrices <- cbind(portfolioPrices,
+                           getSymbols(ticker, from='2009-01-03', periodicity = 'daily', auto.assign=FALSE)[,4])
+}
+
+portfolioReturns <- na.omit(ROC(portfolioPrices))
+
+portf <- portfolio.spec(colnames(portfolioReturns))
+
+portf <- add.constraint(portf, type="weight_sum", min_sum=1, max_sum=1)
+portf <- add.constraint(portf, type="box", min=0, max=.50)
+portf <- add.objective(portf, type="return", name="mean")
+portf <- add.objective(portf, type="risk", name="StdDev")
+
+optPort <- optimize.portfolio(portfolioReturns, portf, optimize_method = "ROI", trace=TRUE)
+
+chart.Weights(optPort)
+
+ef <- extractEfficientFrontier(optPort, match.col = "StdDev", n.portfolios = 25,
+                               risk_aversion = NULL)
+
+chart.EfficientFrontier(ef,
+                        match.col = "StdDev", n.portfolios = 25, xlim = NULL, ylim = NULL,
+                        cex.axis = 0.8, element.color = "darkgray", main = "Efficient Frontier",
+                        RAR.text = "SR", rf = 0, tangent.line = TRUE, cex.legend = 0.8,
+                        chart.assets = TRUE, labels.assets = TRUE, pch.assets = 21,
+                        cex.assets = 0.8)
